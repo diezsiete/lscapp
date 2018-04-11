8de08516d72fed793b9d11e19e14892eb43acab9
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2018-04-11 15:20:29 -0500

    take picture practice funcionalidad de camra separada en clase

--- app/src/main/java/com/diezsiete/lscapp/activity/PracticeActivity.java
+++ app/src/main/java/com/diezsiete/lscapp/activity/PracticeActivity.java
@@ -3,6 +3,7 @@ package com.diezsiete.lscapp.activity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.diezsiete.lscapp.R;
 import com.diezsiete.lscapp.fragment.PracticeFragment;
@@ -78,5 +80,19 @@ public class PracticeActivity extends AppCompatActivity {
             return;
         }
         mPracticeFragment = PracticeFragment.newInstance(mLevelId);
+    }
+    private  static final int REQUEST_CAMERA_RESULT = 106;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case  REQUEST_CAMERA_RESULT:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Cannot run application because camera service permission have not been granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 }
